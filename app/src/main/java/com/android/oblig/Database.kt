da6989f5dd3c46cee4eb56acc2256bd2df6c01e4
Petter A. Salberg <petter_salberg@hotmail.com>
2019-01-17 11:30:09 +0100

    har gjort endringer, opprettet activity database

---
+++ app/src/main/java/com/android/oblig/Database.kt
@@ -0,0 +1,12 @@
+package com.android.oblig
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Database : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_database)
+    }
+}
