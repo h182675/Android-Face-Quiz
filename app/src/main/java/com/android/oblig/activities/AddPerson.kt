39093d09b0019eacf0295dc77ce4a0723b7549c4
Petter A. Salberg <petter_salberg@hotmail.com>
2019-01-25 15:51:35 +0100

    her er en ny push

--- app/src/main/java/com/android/oblig/activities/AddPerson.kt
+++ app/src/main/java/com/android/oblig/activities/AddPerson.kt
@@ -1,12 +1,13 @@
 package com.android.oblig.activities
 
-import android.support.v7.app.AppCompatActivity
+
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.TextInputEditText
+import android.support.v7.app.AppCompatActivity
 import android.widget.Button
-import android.widget.Toast
 import com.android.oblig.R
-import com.android.oblig.modules.Person
+
 
 class AddPerson : AppCompatActivity() {
 
@@ -14,24 +15,46 @@ class AddPerson : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_person)
 
-        //Access the input and button fields
+        /*
+        Access the input and button fields
+         */
         val etInput = findViewById<TextInputEditText>(R.id.navnInputId)
         val submit = findViewById<Button>(R.id.buttonId)
+        val choose = findViewById<Button>(R.id.chooseImageId)
 
-        //When button is clicked...
-        if(submit != null && etInput != null ) {
+        fun selectImageAlbum(){
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "image/*"
+            if(intent.resolveActivity(packageManager) != null){
+                startActivityForResult(intent, REQUEST_SELECT_IMAGE_IN_ALBUM)
+
+            }
+        }
+
+        companion object{
+            private val REQUEST_SELECT_IMAGE_IN_ALBUM = 1
+        }
+        //When choose image button is clicked...
+        if(choose != null){
+            choose.setOnClickListener{
+
+            }
+        }
+        //When add button is clicked...
+        if (submit != null && etInput != null) {
             submit.setOnClickListener {
                 val input = etInput.text.toString()
                 //add picture and name to the database
+
             }
         }
-        //var p = etInput.toString()
 
         //Add person to the database
         fun addPerson(p: String): String {
 
             return p
         }
+
     }
 
 }
