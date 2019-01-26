ae7d26ce0b1e9ac8898b8eb83152ed1999c42532
SenatorCheese <d.pettersen@gmail.com>
2019-01-26 20:55:06 +0100

    Made list print database. TODO: Make new adapter to print more complicated view

--- app/src/main/java/com/android/oblig/activities/PersonList.kt
+++ app/src/main/java/com/android/oblig/activities/PersonList.kt
@@ -1,9 +1,11 @@
 package com.android.oblig.activities
 
 
+import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
@@ -16,36 +18,34 @@ class PersonList : AppCompatActivity() {
     var listItems:MutableList<Person> = ArrayList<Person>()
 
 
+    private lateinit var listView: ListView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_person_list)
 
-        // Create view objects
-        var listView:ListView = findViewById<ListView>(R.id.person_list)
-        var addPersonBtn:Button = findViewById<Button>(R.id.add_person_btn)
-
         // Get the person list from db
         listItems = MainMenu.db.personDao().getAll() as MutableList<Person>
 
-        var p1:Person = Person(1,"guy",null)
-        listItems.add(p1)
+        var personNames = mutableListOf<String>()
+        for(i in 0 until listItems.size){
+            personNames.add(listItems[i].name)
+        }
+
+        // Get view from id
+        var listView:ListView = findViewById(R.id.person_list)
 
         // Create adapter
-        var adapter = ArrayAdapter<Person>(this,android.R.layout.simple_list_item_1,listItems)
-
+        var personAdapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,personNames)
 
         // Link adapter to view
-        listView.adapter
+        listView.adapter = personAdapter
 
+        var addPersonBtn:Button = findViewById<Button>(R.id.add_person_btn)
         addPersonBtn.setOnClickListener {
             val intent = Intent(this,AddPerson::class.java)
             startActivity(intent)
         }
     }
-
-    fun addItems(view:View){
-
-    }
-
 }
+
 
